be2e39ab2ea3369ca1104e92dc620d67655c755e
kziad551 <96523129+kziad551@users.noreply.github.com>
2025-02-27 17:37:52 +0200

    new fixes

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -29,6 +29,7 @@ export class AppSession implements HydrogenSession {
         secrets,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 60 * 60 * 24 * 30, // 30 days
+        domain: process.env.NODE_ENV === 'production' ? '.mariobologna.moda' : undefined,
       },
     });
 
