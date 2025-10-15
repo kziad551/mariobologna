87b2cdc0ba313a2ab832625ab2cd77308b98d4a1
kziad551 <96523129+kziad551@users.noreply.github.com>
2025-10-15 16:57:52 +0300

    Add Microsoft clarity

--- app/root.tsx
+++ app/root.tsx
@@ -436,6 +436,16 @@ export default function App() {
                 });
               `}
             </script>
+            {/* Microsoft Clarity */}
+            <script nonce={nonce}>
+              {`
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "tqkg79j9e5");
+              `}
+            </script>
           </Helmet>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
